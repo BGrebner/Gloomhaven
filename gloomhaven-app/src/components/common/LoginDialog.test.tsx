37f8201355918ff8675b7f3e5586fedc43696314
Benjamin Grebner <bgrebner@gmail.com>
2020-11-06 15:10:33 -0500

    remove old logo file.  Add login with google button option.

--- gloomhaven-app/src/components/common/LoginDialog.test.tsx
+++ gloomhaven-app/src/components/common/LoginDialog.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import LoginDialog from "./LoginDialog";
+import { JsxEmit } from "typescript";
 
 it("should contain login link for google", () => {
-    render(<LoginDialog />);
+    render(<LoginDialog open={true} onClose={jest.fn()} />);
 
     const linkElement = screen.getByText(/google/i);
 
--- gloomhaven-app/src/components/common/LoginDialog.tsx
+++ gloomhaven-app/src/components/common/LoginDialog.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Dialog, List, Button } from "@material-ui/core";
 
-const LoginDialog: React.FC = () => {
-    return (<></>);
+const LoginDialog: React.FC<{open: boolean; onClose: () => void}>  = ({open, onClose}) => {
+    const handleClose = () => onClose();
+
+    return (
+        <Dialog onClose={handleClose} open={open}>
+            <List>
+                <Button>Sign in with Google</Button>
+            </List>
+        </Dialog>
+    );
 }
 
 export default LoginDialog;
