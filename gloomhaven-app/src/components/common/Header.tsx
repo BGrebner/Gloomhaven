423d93c5603682d6fd8992c4a44a21edf6c8beea
Benjamin Grebner <bgrebner@gmail.com>
2020-11-06 14:25:52 -0500

    create header with login button

--- gloomhaven-app/src/components/common/Header.tsx
+++ gloomhaven-app/src/components/common/Header.tsx
@@ -1,7 +1,26 @@
+import { AppBar, Button, createStyles, makeStyles, Theme, Typography } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
 const Header = () => {
-    return <></>
+    const classes = useStyles();
+
+    return (
+    <AppBar position="static">
+        <Typography variant="h6" className={classes.title}>Gloomhaven Campain Keeper</Typography>
+        <Button color="inherit">Login</Button>
+    </AppBar>
+    );
 }
 
 export default Header;
