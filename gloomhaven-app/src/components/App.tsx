200ffd7b85a94fcdbdb9df94ed69f9324a225bca
Benjamin Grebner <bgrebner@gmail.com>
2020-11-06 14:39:39 -0500

    add header into app.  install pretier into app.

--- gloomhaven-app/src/components/App.tsx
+++ gloomhaven-app/src/components/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { Container } from "@material-ui/core";
 import { Route, Switch } from "react-router-dom";
-import HomePage from './home/HomePage';
+import HomePage from "./home/HomePage";
+import Header from "./common/Header";
 
 function App() {
   return (
     <Container>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
       </Switch>
